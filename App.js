b55678c22675f0c9570e42cc98e01b9833e2627f
kaitlynkreth <qte7kg@virginia.edu>
2023-12-12 18:50:22 +0100

    these four now

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from './screens/WelcomeScreen';
+import NameScreen from './screens/NameScreen';
+import DateOfBirthScreen from './screens/DateOfBirth';
+import ProfilePictureScreen from './screens/ProfilePicture';
+import LocationScreen from './screens/Location';
+import PurposeScreen from './screens/Purpose';
+import FinalProfileScreen from './screens/FinalProfile';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ActionSheetProvider> 
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Name" component={NameScreen} />
+        <Stack.Screen name="DateOfBirth" component={DateOfBirthScreen} />
+        <Stack.Screen name="ProfilePicture" component={ProfilePictureScreen} />
+        <Stack.Screen name="Location" component={LocationScreen} />
+        <Stack.Screen name="Purpose" component={PurposeScreen} />
+        <Stack.Screen name="FinalProfile" component={FinalProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </ActionSheetProvider>
   );
 }
 
